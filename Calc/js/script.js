d2c91795b313e7e9539b737674dc8085567bedc1
Irina7Arina7Sillamae <73960503+Irina7Arina7Sillamae@users.noreply.github.com>
2021-03-05 14:44:20 +0200

    Add files via upload

--- Calc/js/script.js
+++ Calc/js/script.js
@@ -1,22 +1,32 @@
 'use strict';
 
 const inputEur = document.querySelector('#eur'),
-	  inputUsd = document.querySelector('#usd');
+      inputUsd = document.querySelector('#usd'),
+      inputRub = document.querySelector('#rub'),
+      inputTry = document.querySelector('#try'),
+      inputCny = document.querySelector('#cny'),
+      inputGbp = document.querySelector('#gbp');
+     
+      inputEur.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
 
-	  inputEur.addEventListener('input', () => {
-	const request = new XMLHttpRequest();
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
 
-	request.open('GET', 'js/current.json');
-	request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-	request.send();
-
-	request.addEventListener('load', () => {
-		if (request.status === 200) {
-			const data = JSON.parse(request.response);
-			inputUsd.value = (+inputEur.value * data.current.usd).toFixed(2);
-		} else {
-			inputUsd.value = "Что-то пошло не так";
-		}
-	});
+    request.addEventListener('load', () => {
+    if (request.status === 200) {
+    const data = JSON.parse(request.response);
+    inputUsd.value = (+inputEur.value * data.current.usd).toFixed(2);
+    inputRub.value = (+inputEur.value * data.current.rub).toFixed(2);
+    inputTry.value = (+inputEur.value * data.current.try).toFixed(2);
+    inputCny.value = (+inputEur.value * data.current.cny).toFixed(2);
+    inputGbp.value = (+inputEur.value * data.current.gbp).toFixed(2);
+        } else {
+    inputUsd.value = "Что-то пошло не так";
+            inputRub.value = "Что-то пошло не так";
+    }
+    });    
 });
+
 
